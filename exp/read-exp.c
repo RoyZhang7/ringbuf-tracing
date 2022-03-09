9b7eb325c8c55920e5207d5c10b1031d831a1611
RoyZhang7 <zhaoqi.zhang@tufts.edu>
2022-03-09 05:31:28 -0500

    /exp syscall part finalized

--- exp/read-exp.c
+++ exp/read-exp.c
@@ -6,7 +6,10 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
+#define TEST_BUF_SIZE     (1024*1024)
+
 static char readFile[] = "./pi-1m";
+static unsigned char testBuf[TEST_BUF_SIZE+10];
  
 int readTest(void)
 {
@@ -21,7 +24,6 @@ int readTest(void)
     //read file and record time
     struct timeval startTime, endTime;
     gettimeofday(&startTime, NULL);
-    for(int i=0; i<TEST_CNT; ++i)
     int tmp = read(fd_read, testBuf, TEST_BUF_SIZE);
     close(fd_read);
     gettimeofday(&endTime, NULL);
@@ -29,7 +31,7 @@ int readTest(void)
 
     //calculate time
     int readTotalTime = (endTime.tv_sec * 1000000 + endTime.tv_usec) - (startTime.tv_sec * 1000000 + startTime.tv_usec);
-    printf("readTotalTime  %f byte.\n", (float)readTotalTime/1000000);
+    printf("readTotalTime  %f s.\n", (float)readTotalTime/1000000);
     return 0;
 }
  
