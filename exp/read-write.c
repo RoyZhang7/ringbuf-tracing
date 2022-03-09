bfa5db569d0af70d269de7f010d78c254db56b91
RoyZhang7 <royzhangno7@gmail.com>
2022-03-09 04:17:12 -0500

    /exp syscall part init

---
+++ exp/read-write.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+ 
+#define TEST_CNT          (100)
+#define TEST_BUF_SIZE     (1024*1024)
+ 
+static char readFile[] = "./read.file";
+static char writeFile[] = "./write.file";
+static unsigned char testBuf[TEST_BUF_SIZE+10];
+ 
+int speedTest(void)
+{
+    //1.read data
+    int fd_read = open(readFile, O_RDONLY);
+    if (fd_read == -1)
+    {
+        printf("failed to open read file:%s\n", readFile);
+        return 0;
+    }
+    int tmp = read(fd_read, testBuf, TEST_BUF_SIZE);
+    printf("read %d byte.\n", tmp);
+    close(fd_read);
+ 
+    //2.write data and get write used time
+    int fd_write = open(writeFile, O_CREAT | O_RDWR);
+    if (fd_write == -1)
+    {
+        printf("failed to open write file:%s\n", writeFile);
+        return 0;
+    }
+ 
+    struct timeval startTime, endTime;
+    gettimeofday(&startTime, NULL);
+    for(int i=0; i<TEST_CNT; ++i)
+    {
+        write(fd_write, testBuf, TEST_BUF_SIZE);
+    }
+    gettimeofday(&endTime, NULL);
+ 
+    //3.calculate speed
+    int writeTotalSize = TEST_BUF_SIZE * TEST_CNT;
+    int writeTotalTime = (endTime.tv_sec * 1000000 + endTime.tv_usec) - (startTime.tv_sec * 1000000 + startTime.tv_usec);
+ 
+    printf("writeTotalSize:%f MB, writeTotalTime:%f s, writeSpeed = %f MB/s.\n",
+        (float)writeTotalSize/1024/1024, (float)writeTotalTime/1000000, ((float)writeTotalSize/1024/1024)/(writeTotalTime/1000000));
+ 
+    close(fd_write);
+    remove(writeFile);
+    return 0;
+}
+ 
+int main()
+{
+	int ret = speedTest();
+    return ret;
+}
